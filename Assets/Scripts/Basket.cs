250a1c6cbe91b825f74296df0cb4cad961424a37
ntcong1905-cyber <kiennt1905@gmail.com>
2026-03-12 14:04:07 +0700

    thay asset, code them logic game

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    public int targetNumber; // Nhập 1, 2, hoặc 3 tương ứng trong Inspector
+    public int targetNumber; 
+    
+    [Header("Cấu hình hình ảnh")]
+    public Sprite fullBasketSprite; // Kéo hình rổ có quả vào đây trong Inspector
+
+    // Hàm này sẽ được gọi từ DragFruit khi kéo đúng quả
+    public void UpdateToFullSprite()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr != null && fullBasketSprite != null)
+        {
+            sr.sprite = fullBasketSprite;
+        }
+    }
 }
